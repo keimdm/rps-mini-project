e31e3833e62ebb845d412f0a68bdf53618d6ba1e
David Keim <dmk252@cornell.edu>
2023-02-08 13:56:10 -0500

    pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//declare variables - array with options, number of wins, losses, ties, game playing
+
+//ask user for input (rp or s)
+//validate user input - if bad, loop back
+
+//generate computer choice
+
+//determine outcome
+
+//display outcome and previous record
+
+//prompt user to play again
+//if yes  loop back
